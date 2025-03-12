05abd4f38e0e806a8d5bc337357845bc22dfb64a
김민결 <neomingyeol@gmail.com>
2025-03-12 15:11:30 +0900

    collider 검사

---
+++ Assets/Scripts/CircleSpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject colliderCirclePrefab;
+    [SerializeField] private Transform moveCircleTransform;
+    [SerializeField] private Transform stopCircleTransform;
+
+    private void Spawn100()
+    {
+        for(int i =0; i < 50; i ++)
+        {
+
+        }
+        for (int i = 0; i < 50; i++)
+        {
+
+        }
+    }
+    private void Spawn1000()
+    {
+        for (int i = 0; i < 500; i++)
+        {
+
+        }
+        for (int i = 0; i < 500; i++)
+        {
+
+        }
+    }
+    private void Spawn10000()
+    {
+        for (int i = 0; i < 5000; i++)
+        {
+
+        }
+        for (int i = 0; i < 5000; i++)
+        {
+
+        }
+    }
+}
